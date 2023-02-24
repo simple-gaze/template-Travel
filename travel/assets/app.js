97b980b3b977204e114d6c07cc7c30988aba05c4
simple-gaze <begishev.a.r@gmail.com>
2023-02-24 10:57:03 +0300

    feat: desktop version finished

---
+++ travel/assets/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    infinite: true,
+    speed: 300,
+    variableWidth: true
+  });
+});
+
+
+
+
+	
