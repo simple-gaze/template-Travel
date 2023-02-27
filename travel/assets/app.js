a9ce150b69f6609dc3bf1eb4bafc7a9681e93af4
simple-gaze <begishev.a.r@gmail.com>
2023-02-27 16:43:11 +0300

    feat: add pop up

--- travel/assets/app.js
+++ travel/assets/app.js
@@ -1,3 +1,4 @@
+// slider
 $(document).ready(function(){
   $('.slider-nav').slick({
     slidesToShow: 1,
@@ -12,6 +13,27 @@ $(document).ready(function(){
   });
 });
 
+// log in and sign up pop up
+$('.open-popup').click(function() {
+  $('.popup-bg').fadeIn(200);
+  $('.popup-login').fadeIn(200);
+});
+
+$('.close-popup').click(function() {
+  $('.popup-bg').fadeOut(200);
+  $('.popup-login').fadeOut(200);
+  $('.popup-signup').fadeOut(200);
+});
+
+$('.open-signup').click(function() {
+  $('.popup-signup').fadeIn(200);
+  $('.popup-login').fadeOut(200);
+});
+
+$('.open-login').click(function() {
+  $('.popup-login').fadeIn(200);
+  $('.popup-signup').fadeOut(200);
+});
 
 
 
