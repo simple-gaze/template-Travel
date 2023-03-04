6f068525a5a92650b20674133c5ed0e09ee89f74
simple-gaze <begishev.a.r@gmail.com>
2023-03-04 21:58:05 +0300

    feat: project finished

--- travel/assets/app.js
+++ travel/assets/app.js
@@ -1,9 +1,31 @@
-// slider
+	/* Menu nav toggle */
+	$("#nav_toggle").on("click", function(event) {
+		event.preventDefault();
+
+		$(this).toggleClass("active");
+		$("#header__navbar").toggleClass("active");
+	});
+
+// slider for desktop
 $(document).ready(function(){
   $('.slider-nav').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    infinite: true,
+    speed: 300,
+    variableWidth: true
+  });
+});
+
+// slider for mobile
+$(document).ready(function(){
+  $('.slider-nav--mobile').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
     dots: true,
     centerMode: true,
     focusOnSelect: true,
